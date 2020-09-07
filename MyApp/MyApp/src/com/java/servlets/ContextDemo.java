5e6fdc66c2719b1550fafb69ac09760b67a4cc32
Rajni Mehra <rajnimehra304@gmail.com>
2020-09-07 20:50:23 +0530

    Uploading Project

---
+++ MyApp/MyApp/src/com/java/servlets/ContextDemo.java
@@ -0,0 +1,32 @@
+package com.java.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ContextDemo
+ */
+//@WebServlet("/ContextDemo")
+public class ContextDemo extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("hello");
+		
+		ServletContext ctx = getServletContext();
+		String str = ctx.getInitParameter("name");
+		out.println(str);
+		
+		// str = ctx.getInitParameter("Phone");
+		//out.println(str);
+	
+	}
+
+}
---
+++ MyApp/src/servlets/ConfigDemo.java
@@ -0,0 +1,31 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+//import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ConfigDemo
+ */
+@WebServlet("/ConfigDemo")
+public class ConfigDemo extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("hello");
+		
+		ServletConfig cg = getServletConfig();
+		String str = cg.getInitParameter("name");
+		out.println(str);
+		
+		
+	}
+
+}
